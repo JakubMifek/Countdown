c6eb229d37c85a9bd99ebf6a67ef0b1685f2da53
VALENTÝNKA\jakub <jakubmifek@gmail.com>
2017-02-23 08:36:56 +0100

    Countdown 1.0

--- Countdown/MainWindow.xaml.cs
+++ Countdown/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Countdown
 {
@@ -23,6 +24,62 @@ namespace Countdown
         public MainWindow()
         {
             InitializeComponent();
+            RegisterInStartup();
+            Left = (int)(SystemParameters.PrimaryScreenWidth / 2.65);
+            Run();
+        }
+
+        private readonly DateTime _final = new DateTime(2017, 5, 18, 23, 59, 59);
+
+        private async void Run()
+        {
+            if (_final > DateTime.Now)
+            {
+                var between = _final - DateTime.UtcNow;
+
+                days.Text = between.Days < 10 ? $"0{between.Days}" : $"{between.Days}";
+                days.Foreground = between.Days > 0 ? Brushes.LawnGreen : Brushes.Red;
+
+                hours.Text = between.Hours < 10 ? $"0{between.Hours}" : $"{between.Hours}";
+                hours.Foreground = between.Days > 0 ? Brushes.White : Brushes.Red;
+
+                mins.Text = between.Minutes < 10 ? $"0{between.Minutes}" : $"{between.Minutes}";
+                mins.Foreground = between.Days > 0 ? Brushes.White : Brushes.Red;
+
+                secs.Text = between.Seconds < 10 ? $"0{between.Seconds}" : $"{between.Seconds}";
+                secs.Foreground = between.Days > 0 ? Brushes.White : Brushes.Red;
+
+                await Task.Delay(200);
+                Run();
+            }
+            else
+            {
+                days.Text = "0";
+                days.Foreground = Brushes.Red;
+
+                hours.Text = "0";
+                hours.Foreground = Brushes.Red;
+
+                mins.Text = "0";
+                mins.Foreground = Brushes.Red;
+
+                secs.Text = "0";
+                secs.Foreground = Brushes.Red;
+            }
+        }
+
+        private void RegisterInStartup()
+        {
+            try
+            {
+                using (RegistryKey registryKey = Registry.CurrentUser.OpenSubKey
+                    ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+                    registryKey?.SetValue("Countdown", $"\"{AppDomain.CurrentDomain.BaseDirectory}\\Countdown.exe\"");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
